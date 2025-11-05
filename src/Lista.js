36f1ff0ec86735d9bcd937cc44010855de5da208
Zoe <zoebrambilla123@gmail.com>
2025-11-05 18:27:56 -0300

    Intento de 3 bis

--- src/Lista.js
+++ src/Lista.js
@@ -1,42 +1,62 @@
-//funcion constructora de la lista de tareas
-function Lista()
-{
-    this.tareas = [];
-}
+import fs from 'node:fs';
+import path from 'node:path';
+import { fileURLToPath } from 'node:url';
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
 
-Lista.prototype.agregar = function(tarea)
-{
-    this.tareas.push(tarea);
-}
+const RUTA_TASKS = path.join(__dirname, 'tasks.json');// se crea la ruta al archivo tasks.json
 
-Lista.prototype.mostrar = function()
-{
-    if (this.tareas.length === 0)
-    {
-        console.log("No hay tareas en la lista.");
-        return;
+    let tasks = [];// es el "plan B" en caso de que falle la carga del archivo
+
+    const tareasLeidas = fs.readFileSync(RUTA_TASKS, 'utf-8');//leo el archivo tasks.json
+
+    try {
+        const data = JSON.parse(tareasLeidas);//convertir el string de vuelta a un objeto
+        if (Array.isArray(data)) tasks = data;//si es un array, lo asigno a tasks
+        else console.warn("El archivo tasks.json no contiene un array válido.");
+    } catch (error) {
+        console.error("Error al cargar 'tasks.json'. ¿Es JSON válido?", error.message);
     }
-    else
+
+    //funcion constructora de la lista de tareas
+    function Lista()
     {
-        console.log("\n--- LISTA DE TAREAS ---");
-        this.tareas.forEach((tarea, index) => {
-            console.log(`\nTarea ${index + 1}:`);
-            tarea.mostrar();
-        });
+        this.tareas = tasks; //asignando una referencia 
     }
-};
 
-Lista.prototype.buscar = function(titulo)
-{
-     const encontrada = this.tareas.find(t => t.titulo.toLowerCase() === titulo.toLowerCase());
+    Lista.prototype.agregar = function(tarea)
+    {
+        this.tareas.push(tarea);
+    }
 
-      if (encontrada) {
-    console.log("🔍 Tarea encontrada:");
-    encontrada.mostrar();
-  } else {
-    console.log("❌ No se encontró ninguna tarea con ese título.");
-  }
-};
+    Lista.prototype.mostrar = function()
+    {
+        if (this.tareas.length === 0)
+        {
+            console.log("No hay tareas en la lista.");
+            return;
+        }
+        else
+        {
+            console.log("\n--- LISTA DE TAREAS ---");
+            this.tareas.forEach((tarea, index) => {
+                console.log(`\nTarea ${index + 1}:`);
+                tarea.mostrar();
+            });
+        }
+    };
 
-export const lista = new Lista();
+    Lista.prototype.buscar = function(titulo)
+    {
+        const encontrada = this.tareas.find(t => t.titulo.toLowerCase() === titulo.toLowerCase());
+
+        if (encontrada) {
+        console.log("🔍 Tarea encontrada:");
+        encontrada.mostrar();
+    } else {
+        console.log("❌ No se encontró ninguna tarea con ese título.");
+    }
+    };
+
+    export const lista = new Lista();
 
--- src/crear.js
+++ src/crear.js
@@ -1,7 +1,7 @@
 import promptSync from "prompt-sync";
 import {Tarea} from "./Tarea.js"
 import {lista} from "./Lista.js"
-
+import fs from 'fs';
 const prompt = promptSync();
 
 export function crear()
@@ -9,15 +9,15 @@ export function crear()
     let titulo = prompt("Título: ");
     let descripcion = prompt("Descripción: ");
     let dif = prompt("Dificultad ([1] baja, [2] media, [3] alta): ");
-        if (dif === "1") dif = "⭐";
-        else if (dif === "2") dif = "⭐⭐";
-        else if (dif === "3") dif = "⭐⭐⭐";
             
         let vencimiento = prompt("Fecha de vencimiento (DD-MM-AAAA): ");
 
         //instanciador de la nueva tarea
         let nuevaTarea = new Tarea(titulo, descripcion, dif, vencimiento);
         lista.agregar(nuevaTarea);
+
+        let jsonString = JSON.stringify(nuevaTarea);
+        fs.writeFileSync('tasks.json', jsonString);
         
         console.log("✅ Tarea agregada exitosamente.");
 }
